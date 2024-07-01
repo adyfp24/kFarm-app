46200295581dd5bd4a92199096da3307ada1f5ff
adyfp24 <adyfp24@gmail.com>
2024-07-01 11:24:21 +0000

    feat: integration for water controlling

--- lib/view_models/water_provider.dart
+++ lib/view_models/water_provider.dart
@@ -1,12 +1,23 @@
 part of 'provider.dart';
 
-class WaterProvider extends ChangeNotifier{
+class WaterProvider extends ChangeNotifier {
   bool _isLoading = false;
   bool get isLoading => _isLoading;
   Watering? _watering;
   Watering? get watering => _watering;
 
-  Future<void> updateWatering(bool control) async{
-
+  Future<void> updateWatering(bool control) async {
+    final response = await http.put(
+        Uri.parse(
+            'https://smartfarm-app-4f440-default-rtdb.asia-southeast1.firebasedatabase.app/watering.json'),
+        body: jsonEncode(control));
+    if (response.statusCode == 200) {
+      _watering = Watering(control);
+      _isLoading = false;
+      notifyListeners();
+    } else {
+      print('gagal menyalakan air');
+      notifyListeners();
+    }
   }
 }
